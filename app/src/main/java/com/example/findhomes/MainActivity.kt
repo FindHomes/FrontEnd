c1b5bd159568c2368833a3894d1b0c483978952d
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-05-13 22:05:14 +0900

    feat: 초기 환경 구축
    :white_check_mark: 초기 환경 세팅

---
+++ app/src/main/java/com/example/findhomes/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.findhomes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.findhomes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(binding.root)
+    }
+}
