05a0a30611c0d30ca939cf13cdf8630f66fb6732
TaejungKim <tae0803@naver.com>
2024-07-10 21:25:02 +0900

    feat : 검색 화면 구현 중

--- app/src/main/java/com/example/findhomes/search/ContractSelectActivity.kt
+++ app/src/main/java/com/example/findhomes/search/ContractSelectActivity.kt
@@ -1,6 +1,7 @@
 package com.example.findhomes.search
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,17 +17,22 @@ class ContractSelectActivity : AppCompatActivity() {
     private lateinit var btnThree: Button
     private lateinit var btnOffice: Button
     private lateinit var btnApart: Button
+    private lateinit var btnMonthly : Button
+    private lateinit var btnCharter : Button
+    private lateinit var btnTrading : Button
+    private var priceAdapter : ContractPriceAdapter ?= null
     lateinit var binding : ActivityContractSelectBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityContractSelectBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
 
-        initHousingType()
+        initRoomType()
+        initContractType()
 
         setContentView(binding.root)
     }
 
-    private fun initHousingType() {
+    private fun initRoomType() {
         btnOne = binding.btnEssentialCategoryOne
         btnTwo = binding.btnEssentialCategoryTwo
         btnThree = binding.btnEssentialCategoryThree
@@ -41,7 +47,33 @@ class ContractSelectActivity : AppCompatActivity() {
         }
     }
 
-    private fun initContractForm() {
+    private fun initContractType() {
+        btnMonthly = binding.btnContractMonthly
+        btnCharter = binding.btnContractCharter
+        btnTrading = binding.btnContractTrading
 
+        //초기 진입 화면은 월세와 전세가 선택된 상황
+        btnMonthly.isSelected
+        btnCharter.isSelected
+        btnMonthly.setTextColor(resources.getColorStateList(R.color.button_selector_text_color, null))
+        btnCharter.setTextColor(resources.getColorStateList(R.color.button_selector_text_color, null))
+
+
+        listOf(btnMonthly, btnCharter, btnTrading).forEach{ button ->
+            button.setOnClickListener {
+                button.setTextColor(resources.getColorStateList(R.color.button_selector_text_color, null))
+                it.isSelected = !it.isSelected
+                if(it == btnMonthly && it.isSelected){
+                    binding.clContractMonthly.visibility = View.VISIBLE
+                } else if (it == btnMonthly && !it.isSelected) {
+                    binding.clContractMonthly.visibility = View.GONE
+                }
+                if(it == btnCharter && it.isSelected){
+                    binding.clContractCharter.visibility = View.VISIBLE
+                }else if (it == btnCharter && !it.isSelected) {
+                    binding.clContractCharter.visibility = View.GONE
+                }
+            }
+        }
     }
 }
