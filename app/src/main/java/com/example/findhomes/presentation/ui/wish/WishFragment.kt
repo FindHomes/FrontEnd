ad898652b8d9fe469ecf568b152b2a64fe0fd05d
TaejungKim <tae0803@naver.com>
2024-10-17 22:47:29 +0900

    feat: Wish 화면 배치
    - Wish 화면의 tabLayout 및 adpater 연동
    
    Related to: #12

--- app/src/main/java/com/example/findhomes/presentation/ui/wish/WishFragment.kt
+++ app/src/main/java/com/example/findhomes/presentation/ui/wish/WishFragment.kt
@@ -6,16 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.findhomes.databinding.FragmentWishBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class WishFragment : Fragment() {
     private lateinit var binding : FragmentWishBinding
+    val tabList = arrayListOf("최근 본 방","찜한 방", "검색 기록")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentWishBinding.inflate(inflater, container, false)
 
+        initView()
 
         return binding.root
+    }
+
+    private fun initView(){
+        binding.vpMain.adapter = TabLayoutVPAdapter(requireActivity())
+        TabLayoutMediator(binding.tbWish, binding.vpMain){ tab, position ->
+            tab.text = tabList[position]
+        }.attach()
     }
 }
