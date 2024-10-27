7128c74cde2e44b46c61bf67c488cba78aab156b
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-10-27 10:08:31 +0900

    chore : 수정 전체 및 지역 데이터 수정
    - 지역 데이터 서버와 동일한 형식으로 수정

--- app/src/main/java/com/example/findhomes/data/dataprovider/DataProvider.kt
+++ app/src/main/java/com/example/findhomes/data/dataprovider/DataProvider.kt
@@ -8,7 +8,7 @@ object DataProvider {
     val cities = listOf(
         //서울
         City("서울", listOf(
-            County("전체"), County("강남구"), County("강동구"), County("강북구"),
+            County("강남구"), County("강동구"), County("강북구"),
             County("강서구"), County("관악구") , County("광진구"), County("구로구"),
             County("금천구"), County("노원구"), County("도봉구"), County("동대문구"),
             County("동작구"), County("마포구"), County("서대문구"), County("서초구"),
@@ -19,7 +19,7 @@ object DataProvider {
 
         //경기
         City("경기", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
+            County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
             County("고양시 일산서구"), County("과천시"), County("광명시"), County("광주시"),
             County("광주시"), County("구리시"), County("군포시"), County("김포시"),
             County("남양주시"), County("동두천시"), County("부천시"), County("성남시 분당구"),
@@ -34,54 +34,58 @@ object DataProvider {
 
         //인천
         City("인천", listOf(
-            County("전체"), County("강화군"), County("계양구"), County("미추홀구"),
+            County("강화군"), County("계양구"), County("미추홀구"),
             County("남동구"), County("동구"), County("부평구"), County("서구"),
             County("연수구"), County("옹진군"), County("중구")
         )),
 
         //부산
         City("부산", listOf(
-            County("전체"), County("강서구"), County("금정구"), County("기장군"),
+            County("강서구"), County("금정구"), County("기장군"),
             County("남구"), County("동구"), County("동래구"), County("부산진구"),
             County("북구"), County("사상구"), County("사하구"), County("서구"),
-            County("수영구"), County("연제구"), County("영도구"), County("중구"),
+            County("수영구"), County("연제구"), County("중구"),
             County("해운대구")
         )),
 
         //대구
         City("대구", listOf(
-            County("전체"), County("군위군"), County("남구"), County("달서구"),
-            County("달성군"), County("동구"), County("북구"), County("서구"),
-            County("수성구"), County("중구")
+            County("중구"), County("동구"), County("서구"),
+            County("남구"), County("북구"), County("수성구"), County("달서구"),
+            County("달성군")
         )),
 
         //광주
         City("광주", listOf(
-            County("전체"), County("광산구"), County("남구"), County("동구"),
+            County("광산구"), County("남구"), County("동구"),
             County("북구"), County("서구")
         )),
 
         //대전
         City("대전", listOf(
-            County("전체"), County("대덕구"), County("동구"), County("서구"),
+            County("대덕구"), County("동구"), County("서구"),
             County("유성구"), County("중구")
         )),
 
         //울산
         City("울산", listOf(
-            County("전체"), County("남구"), County("동구"), County("북구"),
+            County("남구"), County("동구"), County("북구"),
             County("울주군"), County("중구")
         )),
 
         //경남
         City("경남", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("창원시 의창구"), County("창원시 성산구"), County("창원시 마산합포구"),
+            County("창원시 마산회원구"), County("창원시 진해구"), County("진주시"), County("통영시"),
+            County("사천시"), County("김해시"), County("밀양시"), County("거제시"),
+            County("양산시"), County("의령군"), County("함안군"), County("창녕군"),
+            County("고성군"), County("남해군"), County("하동군"), County("산청군"),
+            County("함양군"), County("거창군"), County("합천군")
         )),
 
         //경북
         City("경북", listOf(
-            County("전체"), County("경산시"), County("경주시"), County("고령군"),
+            County("경산시"), County("경주시"), County("고령군"), County("군위군"),
             County("구미시"), County("김천시"), County("문경시"), County("봉화군"),
             County("상주시"), County("성주군"), County("안동시"), County("영덕군"),
             County("영양군"), County("영주시"), County("영천시"), County("예천군"),
@@ -91,44 +95,56 @@ object DataProvider {
 
         //충남
         City("충남", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("천안시 동남구"), County("천안시 서북구"), County("공주시"),
+            County("보령시"), County("아산시"), County("서산시"), County("논산시"),
+            County("계룡시"), County("당진시"), County("금산군"), County("부여군"),
+            County("서천군"), County("청양군"), County("홍성군"), County("예산군"),
+            County("태안군"),
         )),
 
         //충북
         City("충북", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("청주시 상당구"), County("청주시 서원구"), County("청주시 흥덕구"),
+            County("청주시 청원구"), County("충주시"), County("제천시"), County("보은군"),
+            County("옥천군"), County("영동군"), County("증평군"), County("진천군"),
+            County("괴산군"), County("음성군"), County("단양군")
         )),
 
         //전남
         City("전남", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
-        )),
+            County("목포시"), County("여수시"), County("순천시"),
+            County("나주시"), County("광양시"), County("담양군"), County("곡성군"),
+            County("구례군"), County("고흥군"), County("보성군"), County("화순군"),
+            County("장흥군"), County("강진군"), County("해남군"), County("영암군"),
+            County("무안군"), County("함평군"), County("영광군"), County("장성군"),
+            County("완도군"), County("진도군"), County("신안군")
+            )),
 
         //전북
         City("전북", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("군산시"), County("익산시"), County("정읍시"),
+            County("남원시"), County("김제시"), County("완주군"), County("진안군"),
+            County("무주군"), County("장수군"), County("임실군"), County("순창군"),
+            County("고창군"), County("부안군")
         )),
 
         //강원
         City("강원", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("춘천시"), County("원주시"), County("강릉시"),
+            County("동해시"), County("태백시"), County("속초시"), County("삼척시"),
+            County("홍천군"), County("횡성군"), County("영월군"), County("평창군"),
+            County("정선군"), County("철원군"), County("화천군"), County("양구군"),
+            County("인제군"), County("고성군"), County("양양군")
         )),
 
         //제주
         City("제주", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("제주시"), County("서귀포시")
         )),
 
         //세종
         City("세종", listOf(
-            County("전체"), County("가평군"), County("고양시 덕양구"), County("고양시 일산동구"),
-            County("성북구"), County("성북구"), County("성북구"), County("성북구")
+            County("세종특별자치시")
         )),
     )
 
